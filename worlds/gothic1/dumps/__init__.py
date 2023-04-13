e26a784e3593c876d63b5e513df2438773a81523
Salzkorn <salzkitty@gmail.com>
2023-04-13 02:32:24 +0200

    Added dumped data

---
+++ worlds/gothic1/dumps/__init__.py
@@ -0,0 +1,55 @@
+#####################
+# The YAML files included here are dumped by the game and should not be edited.
+# Their structure is documented below.
+#####################
+# npcs.yaml
+#####################
+# - id: 217
+#   obj: "GRD_217_TORWACHE"
+#   name: "Torwache"
+#   weapon:
+#   - obj: "ITMW_1H_SWORD_01"
+#     name: "grobes Schwert"
+#     amount: 1
+#   - ...
+#   armor: ...
+#   rune: ...
+#   magic: ...
+#   food: ...
+#   potion: ...
+#   doc: ...
+#   misc: ...
+# - ...
+#
+#####################
+# containers.yaml
+#####################
+# - obj: "CHEST"
+#   name: "Chest"
+#   contents:
+#   - obj: "ITMINUGGET"
+#     name: "Erzbrocken"
+#     amount: 25
+#   - ...
+# - ...
+#
+#####################
+# items.yaml
+#####################
+# - obj: "ITMNUGGET"
+#   name: "Erzbrocken"
+#   amount: 1
+# - ...
+#
+#####################
+# objects.yaml
+#####################
+# - "CHAIR_1"
+# - ...
+#
+#####################
+
+
+
+
+pass
